f7d86ad3c864e762c73318bb01921c49fe17d6d5
seokyeo <65169722+seokyeo@users.noreply.github.com>
2020-05-30 14:21:22 +0900

    텍스트 수정
    자잘한 텍스트 수정

--- 프로젝트 시작전 메뉴.cpp
+++ 프로젝트 시작전 메뉴.cpp
@@ -9,7 +9,7 @@ public:
 
     system("mode con:cols=100 lines=30"); //콘솔창 크기 50x30으로 설정
     cout << "\n\n\n\n\n\n\n\n\n";
-	cout << "\t\t\t 창공 프로젝트 미사일팀\n" << endl;
+	cout << "\t\t\t          창공 프로젝트 미사일팀\n" << endl;
  	cout << "\t\t\t      IT정보공학과 201912388 오준혁" << endl;	
  	cout << "\t\t\t      IT정보공학과 201515300 장우석" << endl;	
  	cout << "\t\t\t      IT정보공학과 201918800 정석찬" << endl;	
@@ -21,7 +21,7 @@ public:
     	cout << i << "% 로딩중. . ." << endl;
 		Sleep(20); 
 	}
-	cout << "\n\n\t\t\t        press start\n\n";
+	cout << "\n\n\t\t\t        Press Start\n\n";
 	getchar();
 	system("cls"); // enter 입력후 콘솔창 넘어가기 
 	}	
