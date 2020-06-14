89badca3703be55b490a38009f52099299cb2508
wkddntjr1123 <wkddntjr1123@jbnu.ac.kr>
2020-06-15 00:06:26 +0900

    Create Character class.cpp

---
+++ 움직임 구현 작업/Character class.cpp
@@ -0,0 +1,22 @@
+class Character
+{    
+    private : 
+		int x;	
+    	int y;	
+    	int size_x;		
+		int size_y;
+    	char figure[10];		
+    	char name[8];
+    	bool direction;		
+    
+	    float t_jump;
+	    int leg_m;			
+	    unsigned int t_leg;
+	
+	public :
+		Character();
+
+
+};
+		
+
