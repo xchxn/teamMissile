55f3d744064a2c730f7cefc0d5a81fc50a8664dd
wkddntjr1123 <wkddntjr1123@jbnu.ac.kr>
2020-06-16 10:15:55 +0900

    Update Character class.cpp

--- 움직임 구현 작업/Character class.cpp
+++ 움직임 구현 작업/Character class.cpp
@@ -1,21 +1,27 @@
 class Character
 {    
     private : 
-		int x;	
-    	int y;	
-    	int size_x;		
+		int x;		//캐릭터가 출력될 x좌표
+    	int y;		//캐릭터가 출력될 y좌표
+    	int size_x;		//캐릭터를 출력할 3*3크기
 		int size_y;
-    	char figure[10];		
-    	char name[8];
-    	bool direction;		
+    	char figure[10];	//캐릭터 생김새 배열 (3*3크기 출력될 예정)
+    	char name[8];		//캐릭터 위에 뜰 이름 
+    	bool direction;		//true=우측이동 , false=좌측이동 
     
-	    float t_jump;
-	    int leg_m;			
-	    unsigned int t_leg;
-	
+	    float t_jump;		//중력 구현할 변수 
+	    
 	public :
-		Character();
-
+		Character();	//private 변수들 초기화 예정 
+		void ControlCharacter();
+		
+		//private영역에 있는 변수들 리턴
+		int GetX(){return x;}
+		int GetY(){return y;}
+		int GetSizeX(){return size_x;}
+		int GetSizeY(){return size_y;}
+		char* Getfigure(){return figure;}
+		char* Getname(){return name;}
 
 };
 		
