645e5fbe9cb9d2754dc69cf41587c79868b47529
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2020-06-23 13:21:46 +0900

    Create bag()함수  기본틀 (수정전).cpp

---
+++ bag()함수  기본틀 (수정전).cpp
@@ -0,0 +1,20 @@
+void bag(){
+	int num ;
+	cout << " 가방을 여시겠습니까? " << endl;     // 수정'
+    cout << " 1. 네  " << endl;
+	cout << " 2. 아니오 " << endl;
+	cin >> num;
+	if(num = 1){
+		cout << " 멀꺼내시겠습니까? " << endl;
+		cout << " 1. 사과 " << endl;
+		cout << " 2. 주문서 " << endl; 
+		switch(num){
+			case 1:
+				heal();    // 사과를 먹게 되면 체력 힐       나중에 밑에 힐 함수 만들어야함. 
+			case 2: 
+				strup();  // 주문서를 통한 공격력 강화        나중에 밑에 함수 만들어야함. 
+				
+		}
+		
+	} 
+}
