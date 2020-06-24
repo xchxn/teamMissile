246badea786de9c0d582eca747ce6a4d50490034
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2020-06-24 19:13:45 +0900

    Update attack함수 기본틀( 수정전).cpp

--- attack함수 기본틀( 수정전).cpp
+++ attack함수 기본틀( 수정전).cpp
@@ -9,11 +9,11 @@ void attack(){
 			break; 
 		case 2:
 			cout << "기본 스킬1(데미지: 10)" << endl;
-			skill1(); 
+			basic skill1(); 
 			break;
 		case 3:
 		    cout << "기본 스킬2 (데미지: 15)"  << endl; 
-		    skill2();
+		    basic skill2();
 		    break;
 	}
 }
@@ -27,7 +27,7 @@ void basic attack( )                        //만날시 열리는 전투창으로 이동 나중
 	}
 }
 
-void skill1()                             //만날시 열리는 전투창으로 이동 나중에 
+void basic skill1()                             //만날시 열리는 전투창으로 이동 나중에 
 { 
 	Monster_Hp = Monster_Hp - 10;
 	if(sw){
@@ -36,7 +36,7 @@ void skill1()                             //만날시 열리는 전투창으로 이동 나중에
 	}
 }
 
-void skill2()                              //만날시 열리는 전투창으로 이동 나중에 
+void basic skill2()                              //만날시 열리는 전투창으로 이동 나중에 
 {
 	Monster_Hp = Monster_Hp - 15;
 	if(sw){
