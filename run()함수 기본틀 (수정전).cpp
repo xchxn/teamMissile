bf5f6aeb9e6442c57f277e63d6d787bc3e88c96a
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2020-06-25 14:08:30 +0900

    Update run()함수 기본틀 (수정전).cpp

--- run()함수 기본틀 (수정전).cpp
+++ run()함수 기본틀 (수정전).cpp
@@ -1,9 +1,23 @@
 void run(){
 	int num; 
-	cout << "도 망가시겠습니까? " << endl;      
+	int num_2;
+	cout << "도망가시겠습니까? " << endl;      
 	cout << "1. 네 도망가겠습니다." << endl;
 	cout << "2. 아니요." << endl;
 	cin >> num;
-	selMenu = 5; 
+	switch(num){
+		case 1:
+			selMenu = 5;
+		case 2:
+			cout<<"1.공격하시겠습니까?" << endl;
+			cout<<"2.가방을 여시겠습니까?" << endl;
+			cin >> num_2;
+			if(num_2 == 1){
+				attach();
+			}
+			if(num_2 == 2){
+				bag();
+			}
+	}	
 }
 
