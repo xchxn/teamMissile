0091079673f4ac0b8fd2aec3069a3c064d6fd77b
seokyeo <65169722+seokyeo@users.noreply.github.com>
2020-06-29 11:23:08 +0900

    작업중

--- 엔딩크레딧.cpp
+++ 엔딩크레딧.cpp
@@ -1,9 +1,71 @@
 #include <iostream>
-#include <windows.h> 
+#include <windows.h>
+#include <string>
+
 using namespace std;
+
+string a[6]={"Team Missile","201515300 장우석","201912388 오준혁","201912430 조민서","201918800 정석찬","Thank you"};
+
+void Goto(int x,int y)
+{
+    COORD pos={x,y};
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos);
+}
+void PrintEnding(int n,int x,int y)
+{
+	Goto(x,y);
+	for(int i=0;i<n;i++){
+		cout<<a[i]<<"\n";
+	}
+	Sleep(1000);
+	system("cls");
+} 
 int main()
 {	
-	cout<<"압도적 감사";
-	Sleep(2000);
+
+	string a[6]={"Team Missile","201515300 장우석","201912388 오준혁","201912430 조민서","201918800 정석찬","Thank you"};
+	PrintEnding(4,1,32);
+	Goto(1,32);
+	cout<<a[0];
+	Sleep(1000);
 	system("cls");
+	Goto(1,31);
+	cout<<a[0]<<"\n"<<a[1];
+	Sleep(1000);
+	system("cls");
+	Goto(1,30);
+	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2];
+	Sleep(1000);
+	system("cls");
+	Goto(1,29);
+	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3];
+	Sleep(1000);
+	system("cls");
+	Goto(1,28);
+	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4];
+	Sleep(1000);
+	system("cls");
+	Goto(1,27);
+	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
+	Sleep(1000);
+	system("cls");
+	Goto(1,26);
+	cout<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
+	Sleep(1000);
+	system("cls");
+	Goto(1,25);
+	cout<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
+	Sleep(1000);
+	system("cls");
+	Goto(1,24);
+	cout<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
+	Sleep(1000);
+	system("cls");
+	Goto(1,23);
+	cout<<a[4]<<"\n"<<a[5];
+	Sleep(1000);
+	system("cls");
+	Goto(1,22);
+	cout<<a[5];
+	
 }
