23c88dec7f1e4529211fc865ea78cfce758c847b
seokyeo <65169722+seokyeo@users.noreply.github.com>
2020-06-29 11:24:37 +0900

    함수화

--- 엔딩크레딧.cpp
+++ 엔딩크레딧.cpp
@@ -22,50 +22,10 @@ void PrintEnding(int n,int x,int y)
 } 
 int main()
 {	
-
-	string a[6]={"Team Missile","201515300 장우석","201912388 오준혁","201912430 조민서","201918800 정석찬","Thank you"};
-	PrintEnding(4,1,32);
-	Goto(1,32);
-	cout<<a[0];
-	Sleep(1000);
-	system("cls");
-	Goto(1,31);
-	cout<<a[0]<<"\n"<<a[1];
-	Sleep(1000);
-	system("cls");
-	Goto(1,30);
-	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2];
-	Sleep(1000);
-	system("cls");
-	Goto(1,29);
-	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3];
-	Sleep(1000);
-	system("cls");
-	Goto(1,28);
-	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4];
-	Sleep(1000);
-	system("cls");
-	Goto(1,27);
-	cout<<a[0]<<"\n"<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
-	Sleep(1000);
-	system("cls");
-	Goto(1,26);
-	cout<<a[1]<<"\n"<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
-	Sleep(1000);
-	system("cls");
-	Goto(1,25);
-	cout<<a[2]<<"\n"<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
-	Sleep(1000);
-	system("cls");
-	Goto(1,24);
-	cout<<a[3]<<"\n"<<a[4]<<"\n"<<a[5];
-	Sleep(1000);
-	system("cls");
-	Goto(1,23);
-	cout<<a[4]<<"\n"<<a[5];
-	Sleep(1000);
-	system("cls");
-	Goto(1,22);
-	cout<<a[5];
-	
+	PrintEnding(0,1,32);
+	PrintEnding(1,1,31);
+	PrintEnding(2,1,30);
+	PrintEnding(3,1,29);
+	PrintEnding(4,1,28);
+	PrintEnding(5,1,27);
 }
