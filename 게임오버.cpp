9401c51cbcfc251faa2dad6ec7b5e190909f4637
seokyeo <65169722+seokyeo@users.noreply.github.com>
2020-07-02 11:42:34 +0900

    Update 게임오버.cpp

--- 게임오버.cpp
+++ 게임오버.cpp
@@ -2,34 +2,32 @@
 #include <windows.h>
 using namespace std;
 
-char q[110]={"  бсбсбс        бс        бсбсбсбс    бсбсбсбс     бсбсбсбс   бс      бс  бсбсбсбс  бсбсбсбс  \n"};
+char q[110]={" бсбсбс        бс        бсбсбсбс    бсбсбсбс     бсбсбсбс   бс      бс  бсбсбсбс  бсбсбсбс   \n"};
 char w[110]={"бс             бсбс      бс  бс  бс   бс          бс      бс   бс    бс   бс        бс     бс \n"};
 char e[110]={"бс            бс  бс     бс  бс  бс   бсбсбсбс    бс      бс    бс  бс    бсбсбсбс  бсбсбсбс  \n"};
 char r[110]={"бс    бсбс   бсбсбсбс    бс  бс  бс   бс          бс      бс     бсбс     бс        бс     бс \n"};
 char t[110]={"  бсбс  бс  бс      бс   бс  бс  бс   бсбсбсбс     бсбсбсбс       бс      бсбсбсбс  бс      бс\n"};
 
-int main()
-{	
-	printf("\n");
+void PrintGO()
+{
 	for(int i=0;i<110;i++){ 
 		printf("%c",q[i]);
-		Sleep(5);
 	}
 	for(int i=0;i<110;i++){
 		printf("%c",w[i]);
-		Sleep(5);
 	}
 	for(int i=0;i<110;i++){
 		printf("%c",e[i]);
-		Sleep(5);
 	}
 	for(int i=0;i<110;i++){
 		printf("%c",r[i]);
-		Sleep(5);
 	}
 	for(int i=0;i<110;i++){
 		printf("%c",t[i]);
-		Sleep(5);
 	}
-
+}
+int main()
+{	
+	printf("\t\t");
+	PrintGO();
 }
